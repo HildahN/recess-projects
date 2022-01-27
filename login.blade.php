f406f5d21c7f4c0ab5bffcbb595634a1620e2545
kimera-marvin <98281745+kimera-marvin@users.noreply.github.com>
2022-01-27 12:14:30 +0300

    Add files via upload

---
+++ login.blade.php
@@ -0,0 +1,20 @@
+<title>Log in to KinderCare Application</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="myform.css">
+<body class="sig-bg">
+    <center>
+    <div class="container">
+    <form method="POST" action="main">
+        <p class="text-primary" id="header">KinderCare</p >
+        <label for="username">UserName:</label>
+        <input type="text"><br><br>
+        <label for="password">Password:</label>
+        <input type="password"><br><br>
+        <input type="checkbox" name="remember">
+        <label for="remember">Remember Me</label><br>
+        <button type="submit" class="btn btn-primary" id="up">Log in</button><br><br>
+        <p>Is this your first time here? <a href="sign">signup</a></p>
+</div>
+</center>
+</body>
